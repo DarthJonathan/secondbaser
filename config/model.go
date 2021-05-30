0397f122f5f6266482bd655da49bf78f74adb143
Jonathan Hosea <jonathan.hosea@dana.id>
2021-05-31 01:46:12 +0700

    Missing add

---
+++ config/model.go
@@ -0,0 +1,29 @@
+package config
+
+type Config struct {
+	DBHost string `json:"DB_HOST"`
+
+	DBUser string `json:"DB_USER"`
+
+	DBPassword string `json:"DB_PASSWORD"`
+
+	DBPort string `json:"DB_PORT"`
+
+	DBDatabase string `json:"DB_DATABASE"`
+
+	GRPCPort string `json:"GRPC_PORT"`
+
+	HTTPPort string `json:"HTTP_PORT"`
+
+	RedisPassword string `json:"REDIS_PASSWORD"`
+
+	RedisHost string `json:"REDIS_HOST"`
+
+	ZipkinEndpoint string `json:"ZIPKIN_ENDPOINT"`
+
+	ApplicationEnv string `json:"APP_ENV"`
+
+	KafkaBrokerAddress string `json:"KAFKA_BROKER_ADDRESS"`
+
+	LogLevel string `json:"LOG_LEVEL"`
+}
